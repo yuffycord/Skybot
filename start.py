61c25564526eb088224e869b2263e0edd1d1ff1a
raidesuuu <contact@raic.tech>
2024-12-25 21:48:46 +0900

    feat: .env削除

--- start.py
+++
@@ -1,9 +0,0 @@
-from bot.bot import bot
-from os.path import join, dirname
-from dotenv import load_dotenv
-import os
-
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
-
-bot.run(os.environ.get("TOKEN"))
